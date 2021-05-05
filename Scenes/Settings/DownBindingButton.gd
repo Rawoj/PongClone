9cf26889d6dc8923f9d5bbf9f24ea5ce41df3fbf
Rawoj <aseetx@gmail.com>
2021-05-05 21:38:59 +0200

    cleanup

--- Scenes/Settings/DownBindingButton.gd
+++ Scenes/Settings/DownBindingButton.gd
@@ -8,12 +8,10 @@ func _ready():
 	p1DownLabel = get_node("../DownBinding")
 
 func _on_button_down():
-	p1DownLabel.add_color_override("font_color", Color(255, 24, 24))    #font_color = Color(255, 24, 24) 
 	p1DownLabel.text = "Press any key"
 	waitingForInput = true
 	var key = yield(self, "get_input")
 	waitingForInput = false
-	p1DownLabel.add_color_override("font_color", Color(255, 255, 255))
 	p1DownLabel.text = OS.get_scancode_string(key.scancode)
 	InputMap.action_erase_events("player1_down")
 	InputMap.action_add_event("player1_down", key)
--- Scenes/Settings/UpBindingButton.gd
+++ Scenes/Settings/UpBindingButton.gd
@@ -8,12 +8,10 @@ func _ready():
 	p1UpLabel = get_node("../UpBinding")
 
 func _on_button_down():
-	p1UpLabel.add_color_override("font_color", Color(255, 24, 24))    #font_color = Color(255, 24, 24) 
 	p1UpLabel.text = "Press any key"
 	waitingForInput = true
 	var key = yield(self, "get_input")
 	waitingForInput = false
-	p1UpLabel.add_color_override("font_color", Color(255, 255, 255))
 	p1UpLabel.text = OS.get_scancode_string(key.scancode)
 	InputMap.action_erase_events("player1_up")
 	InputMap.action_add_event("player1_up", key)
